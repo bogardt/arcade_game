22cb852316846f27c585deffd276f59bf39c5c5c
Thomas Bogard <thomas.bogard@epitech.eu>
2018-04-06 20:09:59 +0200

    add sdl source and compil for unix

--- srcs/Main.cpp
+++ srcs/Main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "SDL.h"
 
 int main(int ac, char **av)
 {
+
+    if (SDL_Init(SDL_INIT_VIDEO) == -1)
+    {
+        fprintf(stderr, "->msdl init error\n");
+        return (0);
+    }
+
     std::cout << "test" << std::endl;
+
     return 0;
 }
